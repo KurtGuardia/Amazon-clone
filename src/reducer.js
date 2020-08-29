30a890e818cf31fa79bf66ac066faf3b0b57feb7
Kurt Guardia <kurtguardia@gmail.com>
2020-08-29 20:28:48 +0200

    created the Sign-in interface

--- src/reducer.js
+++ src/reducer.js
@@ -3,12 +3,11 @@ export const initialState = {
 };
 
 // Selector
-export const getBasketTotal = (basket) => 
-    basket?.reduce((amount, item) => item.price + amount, 0);
-
+export const getBasketTotal = (basket) =>
+  basket?.reduce((amount, item) => item.price + amount, 0);
 
 const reducer = (state, action) => {
-    // console.log(action)
+  // console.log(action)
   switch (action.type) {
     case "ADD_TO_BASKET":
       return {
@@ -16,8 +15,24 @@ const reducer = (state, action) => {
         basket: [...state.basket, action.item],
       };
 
-      case "REMOVE_FROM_BASKET":
-          
+    case "REMOVE_FROM_BASKET":
+      const index = state.basket.findIndex(
+        (basketItem) => basketItem.id === action.id
+      );
+      let newBasket = [...state.basket];
+
+      if (index >= 0) {
+        newBasket.splice(index, 1);
+      } else {
+        console.warn(
+          "Can't remove product (id: ${action.id}) as it is not in the basket!"
+        );
+      }
+
+      return {
+        ...state,
+        basket: newBasket,
+      };
 
     default:
       return state;
